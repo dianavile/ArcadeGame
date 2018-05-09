a57b2f66d5e53697689b1e368c1f25abe718dbeb
@dianavile <diana.vile@gmail.com>
2018-05-09 13:16:15 +0200

    Update the enemy's position

--- js/app.js
+++ js/app.js
@@ -12,7 +12,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    if (this.x <= 505) { // Enemies move horizontally (= ONLY x-value changes)
+        this.x += this.speed * dt;
+    } else {
+        this.x = -100; // Enemy off-canvas. Back to left of canvas
+    }
+};    
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
