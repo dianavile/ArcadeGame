d95c042dddb2146f308b9b0d8f5536beb55985f2
@dianavile <diana.vile@gmail.com>
2018-05-09 13:47:34 +0200

    Instantiate Player and Enemy objects
    - Instantiate Player and Enemy objects
    - Place the player object in a variable called player
    - Place all enemy objects in an array called allEnemies

--- js/app.js
+++ js/app.js
@@ -6,6 +6,22 @@ var Enemy = function(x, y, speed) {
     this.sprite = 'images/enemy-bug.png';
 };
 
+Player.prototype.update = function() { // Add keyboard events
+// Turn into if-else statement for boundaries of canvas
+// i.e. if this.y > nnn;
+// this.y = nnn;
+// do this for left, right and bottom
+// Add: top, if player reaches water, reset game
+};
+
+Player.prototype.render = function() { // Draw player on screen
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(e) {
+    this.ctlKey = e;
+};
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
@@ -28,16 +44,24 @@ Enemy.prototype.render = function() { // Rendering (=show Enemy on Canvas)
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function(x, y, speed) {
-     this.x = x; // 200
-     this.y = y; // 400
-     this.speed = speed; // 50
+     this.x = x; 
+     this.y = y; 
+     this.speed = speed; 
      this.sprite = 'char-princess-girl.png'
 +};
 
-// Now instantiate your objects.
+// Instantiate Enemy and Player objects.
+// Place the player object in a variable called player	
+const player = new Player(202.5, 400, 50);
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+const enemyOne = new Enemy(-100, 227.5, 50);
+const enemyTwo = new Enemy(-100, 227.5, 50);
+const enemyThree = new Enemy(-100, 145, 50);
+const enemyFour = new Enemy(-100, 60, 50);
+const enemyFive = new Enemy(-100, 60, 50);
 
+let allEnemies = [];
+allEnemies.push(enemyOne, enemyTwo, enemyThree, enemyFour, enemyFive);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
