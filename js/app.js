39747f3c6dc2fe7aa2d182db052684cb18a510e5
@dianavile <diana.vile@gmail.com>
2018-05-21 12:48:42 +0200

    Update app.js with latest changes

--- js/app.js
+++ js/app.js
@@ -1,115 +1,124 @@
-/* DEFINE GLOBAL VARIABLES 
- * @description Create Counter for points 
- * & Use getElementById() method to define access to points
- */
-let counter = 0;
-const points = document.getElementById('points');
+'use strict'; //whole-script strict mode syntax (from MDN: https://www.w3schools.com/js/js_strict.asp)
+
+let gameStart = false;
+const modal = document.getElementById('myModal');
+const modal_content = document.querySelector('gameRules');
+const btn = document.getElementById("myBtn");
+restartGame();
+if(gameStart === true){      
+    modal.style.display = "none";
+    } else {
+    modal.style.display = "block";                
+    }
 
 /* **************ENEMY SECTION*********** */
-/* CREATE ENEMY class 
- * @description (enemy = bug, that player must avoid)
- * @param Define Variables to instances: x- & y-position on canvas, enemies´ speed, {img} sprite - to load enemies images
- */
-var Enemy = function(x, y) { 
+/* CREATE ENEMY class */
+class Enemy {
+
+  constructor(x,y){
     this.x = x;
     this.y = y;
     this.sprite = 'images/enemy-bug.png'; 
     this.speed = Math.floor((Math.random() * 200) + 100);
-};
+  }
 
-/* UPDATE ENEMY position
- * @description (=required game method)
- * @param {number} dt - a time delta between ticks
- * @param Multiply movement by 'dt' parameter (=required to run same game speed on all computers)
- * @description Detect collision and restart Player position
- */
-Enemy.prototype.update = function(dt) {
-    if (this.x <= 505) { // Enemies move horizontally (= ONLY change x-value)
-        this.x += this.speed * dt;
-         this.x = Math.floor(this.x);
-    } else {
-        this.x = -100; // Enemy off-canvas. Reset Enemy Position Back to left of canvas
-    }
-    checkCollision(this); 
-    if (Math.abs(this.y - player.y) === 0) {
-    if (Math.abs(this.x - player.x) < 75) {
-      player.x = 202;
-      player.y = 404;
-      player.lives--;
-      if (player.lives === 0) {
-        gameOver();
-      }
-    }
-  }  
-}; 
+/* UPDATE ENEMY position */
+// @Param: dt, a time delta between ticks (multiply movement by dt)
+  update(dt) {
+    this.x += this.speed * dt;
+      if (this.x > 500) {this.x = -100;}// Enemies move horizontally (= ONLY change x-value)
+    // Check if player collides with enemy
+        checkCollision(this); 
+      if (player.y === this.y) {
+      if (player.x > this.x - 75 && player.x < this.x + 75) 
+        player.resetPosition();
+     }
+  }
 
-/* DRAW ENEMY on screen
- * @description (=required game method)
- * @param create Render method (=to show Enemy on "ctx"= Canvas) 
- * @param Render Method uses ctx.drawImage() method with  three params: image, x, y
-*/
-Enemy.prototype.render = function() {
+/* DRAW ENEMY on screen*/
+// @param create Render method (=to show Enemy on "ctx"= Canvas) 
+  render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
-}; 
+  }
+}
 
 /* **************PLAYER SECTION*********** */
-/**
- * @description PLAYER
- * Create Player class =Same as Enemy class, 'speed'(=not necessary?) 
- * @param Extra feature: add lives
- * @param Add update() method (=required game method)
- * @param Add render() method  (= to show Player on Canvas(=required game method)
- * @param Add handleInput() method (=required game method)
-  */
-var Player = function(x, y) { //Define Player class
+/* CREATE PLAYER class */
+class Player {
+
+    constructor (x,y){ 
     this.x = x;
     this.y = y;
     this.sprite = 'images/char-cat-girl.png';
     this.lives = 3;
-    this.gameOver = false;
-};
+    this.gameOver = false;    
+  }
 
-Player.prototype.update = function() { //Player update() method
+/* Player update() method */
+ update () { 
     showLives(this.lives); 
-};
+  }
 
-// Draw the player on the screen
-Player.prototype.render = function() { // Draw player on screen
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+// Draw player on the screen
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
+  }
+}
 
-/**
- * @description ARROYKEYS
- * Player handleInput() method for Arrow keys
- * Define allowedKeys 'left', 'right', 'down' & 'up'- see below
- */
+/* **************GEM SECTION*********** */
+/* CREATE GEM class */
+class Gem {
+
+    constructor (x,y){ 
+    this.GemX; 
+    this.GemY;
+    this.sprite = 'images/Gem Orange.png';
+  }
+
+/* GEM UPDATE() method */
+    update () {     
+    let GemX = [0, 100, 200, 300, 400];
+    let GemY = [72, 154, 236];
+      if (this.GemX == undefined || this.GemY == undefined) {
+        this.GemX = GemX[Math.floor(Math.random() * GemX.length)];
+         this.GemY = GemY[Math.floor(Math.random() * GemY.length)];
+     }
+  // Check if player collides with gem
+  //gemCollision(this);
+  }
+ 
+/* RENDER() method to Draw the Gems on the screen*/
+    render() {
+      ctx.drawImage(Resources.get(this.sprite), this.GemX, this.GemY); 
+    }
+  }
+
+/* Player handleInput() method for ARROYKEYS */
+// Define allowedKeys 'left', 'right', 'down' & 'up'- see below 
 Player.prototype.handleInput = function(keyCode) {
- if (keyCode == 'left') { // See allowedKeys below
-        if (this.x - 101 < 0) {
+      if (keyCode == 'left') { // See allowedKeys below
+          if (this.x - 101 < 0) {
             this.x = 0;
-            resetPlayer(); // Player back to start
-            counter = counter + 1; // Players scores extra level
-            points.innerHTML = counter;  // Update points
-        } else {
+          } else {
             this.x -= 101;
-        }
-    }
-    if (keyCode == 'right') {
-        if (this.x + 101 > 404) {
+          }
+      }
+      if (keyCode == 'right') {
+          if (this.x + 101 > 404) {
             this.x = 404;
-        } else {
+          } else {
             this.x += 101;
-        }
-    }
-    if (keyCode == 'down') {
-        if (this.y + 85 > 404) {
+          }
+      }
+          if (keyCode == 'down') {
+          if (this.y + 85 > 404) {
             this.y = 404;
-        } else {
+          } else {
             this.y += 83;
         }
     }
-    if (keyCode == 'up') {
-        if (this.y - 85 < 0) { // Player reaches water
+          if (keyCode == 'up') {
+          if (this.y - 85 < 0) { // Player reaches water
             resetPlayer(); // Player goes back to start
             counter = counter + 1; // Increase Score 
             points.innerHTML = counter; // Update score points in HTML
@@ -117,14 +126,53 @@ Player.prototype.handleInput = function(keyCode) {
             this.y -= 83;
         }
     }
-};
+}; 
+
+/*    const handleInput = (keyCode) => {
+    switch(keyCode){
+      case 'left':
+          (this.x-101<0) ? this.x = 0:this.x-=101;
+            break;
+      case 'right': 
+          (this.x+101>404) ? this.x = 404:this.x+=404;
+            break; 
+      case 'down':
+          (this.y + 85 > 404)? this.y = 404:this.y+=83;
+            break;
+      case 'up':
+          (this.y - 85 < 0) ? this.y = 0:this.y-=83;
+            resetPlayer(); // Player goes back to start
+            counter = counter + 1; // Increase Score 
+            points.innerHTML = counter; // Update score points in HTML
+            break;
+    }
+}
+
+console.log (this.handleInput);
+*/
+
+
+/* @description INSTANTIATE ALL (Enemy, Player and Gem) OBJECTS  */
+//*  Create Counter for points & Use getElementById() method to define access to points
+let counter = 0; 
+const points = document.getElementById('points');
+let player = new Player(202, 404); //Define player (x-)position: (0, 0 is top left corner) & (505 / 2 - 50.5 = 202) 
+let lives = 3;
+
+const newEnemy = new Enemy(-100, 227.5, 50); //Define enemy (y-) position: to create enemies
+const newEnemy1 = new Enemy(-100, 145, 50);
+const newEnemy2 = new Enemy(-100, 61, 50);
+let allEnemies = [newEnemy, newEnemy1, newEnemy2 ]; //Place allEnemy objects in allEnemies[] array 
+
+const gem1 = new Gem();// Instantiate the Gem
+let allGems = [gem1]; // Place all Gems in an array called allGems
 
 /* **************COLLISION SECTION*********** */
-/* @description COLLISION
- * CODE from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+/* CODE from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
  * Create Collision() function- to detect if Player and Enemy(bug) collide 
  * @param Check if 'this' Enemy collides with player
  *        resetPlayer() method to reset Player back to start
+ *        TO ADD: delete 1 live
  *        Increase score (level) with one
  *        Update points
  */
@@ -137,35 +185,52 @@ function checkCollision(oneEnemy) {
         counter = 0;
         console.log(points);
         points.innerHTML = counter; 
+        showLives();
         }
     };
 
 /**
-/*  @description resetPlayer
+ * @description resetPlayer
  * Define resetPlayer() function- to put player position back to x = 202 and y = 404
  */
 function resetPlayer() { 
     player.x = 202;
     player.y = 404;
 };
- 	 
-/**
-/* @description INSTANTIATE Enemy and Player objects.
- * Place player object in variable 
- * Define player (x-)position: (0, 0 is top left corner) & (505 / 2 - 50.5 = 202) 
- * Place enemy object in variable
- * Define enemy (y-) position: to create enemies
- * Place allEnemy objects in allEnemies[] array 
- */
-let player = new Player(202, 404); 
-const newEnemy = new Enemy(-100, 227.5, 50); 
-const newEnemy1 = new Enemy(-100, 145, 50);
-const newEnemy2 = new Enemy(-100, 61, 50);
-/*const newEnemy3 = new Enemy(-100, 227.5, 50);
-const newEnemy4 = new Enemy(-100, 145, 50);
-const newEnemy5 = new Enemy(-100, 61, 50);*/
 
-let allEnemies = [newEnemy, newEnemy1, newEnemy2, /*newEnemy3, newEnemy4, newEnemy5*/];
+/* **************HEARTS=(LIVES) SECTION *********** */
+function showLives() { // Generate html to display hearts-lives
+  let lives = document.querySelector('.hearts');
+  let heartHtml = '<img src="images/Heart.png">';
+  lives.innerHTML = "";
+
+  for (let i = 0; i < 3; i++) {
+     //if (i > 1) {
+    lives.innerHTML += heartHtml;
+    //lives.style.visibility = 'collapse';
+  }
+} 	 
+
+/* **************GEM COLLISION ***********  */
+function gemCollision() {
+   if (this.x > 500) this.x = -100; 
+   gemCollision(this);
+   if (player.y === this.y) {
+   if (player.x > this.x - 75 && player.x < this.x + 75) 
+    player.resetPosition();
+    removeGem();
+    console.log("Delete Gem");
+  }
+};    
+
+function removeGem() {
+  if (gemCollision(this) = true) {
+        counter = (counter + 5);
+        //console.log("Add 5 points");
+        console.log(points);
+        points.innerHTML = counter;
+  }
+}; 
 
 /* @description ARROYKEYS
  * This Event listens for key Presses and send keys to 
@@ -182,110 +247,42 @@ document.addEventListener('keyup', function(e) {
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
-/* **************GEM SECTION (EXTRA FEATURE)*********** */
-/** 
- * @description GEM: Create Gem class
- * Generate random X & Y-coordinates for each Gem
- * Implement gem collection mechanism
- * If player gets gem, remove gem from board
-*/
-
-let gem = {
-  green: 'images/Gem Green.png',
-  blue: 'images/Gem Blue.png',
-  orange: 'images/Gem Orange.png'
+function restartGame() {  
+      
+    btn.onclick = () => {
+        gameStart = true;
+        modal.style.display = "none";
+        modal_content.style.display="none";        
+    }  
 };
 
-var Gem = function(sprite, x, y) {
-  this.sprite = 'images/Gem Green.png'; 
-  this.x = x;
-  this.y = y;
-};
-
-var getGemX = function getX() {
-  var gemX = [0, 100, 200, 300, 400];
-  return gemX[Math.floor(Math.random() * gemX.length)];
-};
-
-var getGemY = function getY() {
-  var gemY = [72, 154, 236];
-  return gemY[Math.floor(Math.random() * gemY.length)];
-};
-
-// Implement gem collection mechanism
-Gem.prototype.update = function() {
-    // Check if all gems are collected
-  if (gem1.x === -500 && gem2.x === -500 && gem3.x === -500) {
-    // If player collects gem, remove it from board  
-    if (Math.abs(this.y - player.y) === 0) {
-      if (Math.abs(this.x - player.x) < 80) {
-        this.x = -500;
-        gameWin();
-      }
+// Select player at Gamestart
+const choosePlayer = (selection) => {
+    switch(selection){
+      case "cat":
+          player.sprite = 'images/char-cat-girl.png';
+            break;
+      case "boy": 
+          player.sprite = 'images/char-boy.png';
+            break; 
+      case "horn":
+          player.sprite = 'images/char-horn-girl.png';
+            break;
+      case "pink":
+          player.sprite = 'images/char-pink-girl.png';
+            break;
+      case "princess":
+          player.sprite = 'images/char-princess-girl.png';
+          break;
     }
-  }
-}     
-// Render (=Draw) the Gem on the screen
-Gem.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Instantiate the Gem
-const gem1 = new Gem('images/Gem Green.png', getGemX(), getGemY());
-const gem2 = new Gem('images/Gem Blue.png', getGemX(), getGemY());
-const gem3 = new Gem('images/Gem Orange.png', getGemX(), getGemY());
-
-// Place all Gems in an allGems[] array
-let allGems = [gem1, gem2, gem3];
-
-// Generate gem
-//var gem = new Gem(getGemX()); // Is this needed? 
-
-/* **************KEY SECTION (EXTRA FEATURE)*********** */
-// Key Class
-var Key = function(x) {
-  this.sprite = 'images/Key.png';
-  this.x = x;
-  this.y = -10;
-};
-
-// Render Key on canvas
-Key.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-// Implement key collection mechanism
-Key.prototype.update = function() {
-  // Check if all gems are collected
-  if (gem1.x === -500 && gem2.x === -500 && gem3.x === -500) {
-    // If player collects key, remove it from board
-    if (Math.abs(this.y - player.y) === 0) {
-      if (Math.abs(this.x - player.x) < 80) {
-        this.x = -500;
-        gameWin();
-      }
-    }
-  }
-};
-
-// Generate key
-var key = new Key(getGemX());
-
-/* **************HEARTS=(LIVES) SECTION (EXTRA FEATURE)*********** */
-// Generate html to display hearts-lives
-function showLives(num) {
-  var lives = document.querySelector('.hearts');
-  var heartHtml = '<img src="images/Heart.png">';
-  lives.innerHTML = "";
-
-  for (let i = 0; i < num; i++) {
-    lives.innerHTML += heartHtml;
-  }
 }
 
-/* **************MODAL SECTIONS*********** */
-/*Declare Modal score settings
- 
+console.log (player.sprite);
+
+
+/* **************MODAL SECTIONS*********** 
+//Declare Modal score settings
+/* 
 const modal = document.getElementById("modal"); // Declare Modal var
 const canvasName = document.getElementById("gameStart"); // Declare Canvas var
 let showModal = document.getElementById("gameComplete");
@@ -294,6 +291,7 @@ let hideModal = document.getElementById("restart");
 let totalMoves = document.getElementById("totalMoves").innerHTML;
 // declare playAgain button on modal
 const playAgain = document.querySelector(".restart");
+
 
 // Display modal game-start when game starts
 document.addEventListener("DOMContentLoaded", gameStart);
@@ -334,5 +332,65 @@ function gameWin() {
   restartButton.addEventListener('click', function() {
     location.reload();
   });
+}
+*/
+
+/* **************KEY SECTION (EXTRA FEATURE)***********
+// Key Class
+var Key = function(x) {
+  this.sprite = 'images/Key.png';
+  this.x = x;
+  this.y = -10;
+};
+
+// Render Key on canvas
+Key.prototype.render = function() {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Implement key collection mechanism
+Key.prototype.update = function() {
+  // Check if all gems are collected
+  if (gem1.x === -500 && gem2.x === -500 && gem3.x === -500) {
+    // If player collects key, remove it from board
+    if (Math.abs(this.y - player.y) === 0) {
+      if (Math.abs(this.x - player.x) < 80) {
+        this.x = -500;
+        gameWin();
+      }
+    }
+  }
+};
+
+// Generate key
+//var key = new Key(getGemX());
+*/ 
+
+/*
+//HERE YOU HAVE AN EXAMPLE FOR THE ENEMY CLEAN OBJECT
+//
+class Monster {
+
+  constructor() {
+    this.x = x;
+    this.y = y;
+    this.sprite = 'images/enemy-bug.png'; 
+    this.speed = Math.floor((Math.random() * 200) + 100);
+  }
+
+  update(dt) {
+    this.x += this.speed * dt;
+    if (this.x > 500) {this.x = -100;}// Enemies move horizontally (= ONLY change x-value)
+  // Check if player collides with enemy
+      checkCollision(this); 
+    if (player.y === this.y) {
+    if (player.x > this.x - 75 && player.x < this.x + 75) 
+      player.resetPosition();
+    }
+  }
+
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y); 
+  }
 }
 */
